7c979cea059f9843bb606db35b4ea9fb0eb2a85f
Toni <>
2024-10-18 10:12:17 +0200

    works

--- src/resolvers/AttackResolver.ts
+++ src/resolvers/AttackResolver.ts
@@ -1,6 +1,8 @@
 import { Resolver, Query, Ctx, Arg } from 'type-graphql';
 import { Attack, AttackWhereInput } from '../types';
 import { AttackService } from '../services/AttackService';
+import { AppDataSource } from '../db/AppDataSource';
+import { UserLog } from '../db/entity';
 @Resolver()
 export class AttackResolver {
   @Query(() => [Attack])
@@ -13,6 +15,15 @@ export class AttackResolver {
   @Query(() => [String])
   async status(
     @Ctx() context: any): Promise<String[]> {
-    return ['JurgenSaidthis', 'JurgenDeniedThis']
+      const repository = AppDataSource.getRepository(UserLog);
+      const count = await repository.count();
+      console.log('COUNTED BATTLE : ', count);
+      const userlog = new UserLog();
+      userlog.timestamp = Math.floor(Math.random() * 1000000);
+      userlog.comment = "Jurgen has a dog";
+      await repository.insert(userlog);
+      const all = await repository.find();
+      return all.map(entry => entry.comment);
   }
+  
 }
