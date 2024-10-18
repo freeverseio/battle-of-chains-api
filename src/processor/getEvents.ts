4181390185d7c481f2478760281cfa07d25af266
Toni <>
2024-10-18 15:42:45 +0200

    creating reasonable events

--- src/processor/getEvents.ts
+++ src/processor/getEvents.ts
@@ -1,25 +1,77 @@
-import { JoinedChainType, RawMultichainMint, Event } from "./types";
+import { JoinedChainType, RawMultichainMint } from "./types";
 
-export async function getEvents(): Promise<Event[]> {
+const reasonableDate = 1729253900;
+const reasonableBlock = 1000;
+
+export async function getJoinedChainEvents(): Promise<JoinedChainType[]> {
   return new Promise((resolve) => {
-    const events: Event[] = [
+    const events: JoinedChainType[] = [
       {
         _user: "0x1111111111111111111111111111111111111111",
-        _homeChain: 1,
+        _homeChain: 127,
         _nickname: "Alice",
-        timestamp: new Date(),
-        blockNumber: 1000,
+        timestamp: new Date(reasonableDate),
+        blockNumber: reasonableBlock,
         blockHash: "0xabc123",
         txHash: "0xdef456",
+        logIndex: 0,
+      },      
+      {
+        _user: "0x2211111111111111111111111111111111111122",
+        _homeChain: 127,
+        _nickname: "Bob",
+        timestamp: new Date(reasonableDate),
+        blockNumber: reasonableBlock,
+        blockHash: "0xabc123",
+        txHash: "0xdef000",
         logIndex: 1,
+      },
+      {
+        _user: "0x3311111111111111111111111111111111111133",
+        _homeChain: 127,
+        _nickname: "Carol",
+        timestamp: new Date(1729253900 + 100),
+        blockNumber: reasonableBlock + 10,
+        blockHash: "0xabc567",
+        txHash: "0xdef789",
+        logIndex: 0,
+      }
+    ];
+    setTimeout(() => resolve(events), 1000); // Simulate a delay
+  });
+}
+export async function getMultichainMintEvents(): Promise<RawMultichainMint[]> {
+  return new Promise((resolve) => {
+    const events: RawMultichainMint[] = [
+      {
+        _tokenId: BigInt(123456789),
+        _user: "0x2222222222222222222222222222222222222222",
+        _type: 42,
+        _homeChain: 2,
+        timestamp: new Date(reasonableDate + 1000),
+        blockNumber: reasonableBlock + 1000,
+        blockHash: "0xghi789",
+        txHash: "0xjkl012",
+        logIndex: 2,
+      },
+      {
+        _tokenId: BigInt(123456789),
+        _user: "0x2211111111111111111111111111111111111122",
+        _type: 42,
+        _homeChain: 2,
+        timestamp: new Date(reasonableDate + 1000),
+        blockNumber: reasonableBlock + 1000,
+        blockHash: "0xghi789",
+        txHash: "0xjkl012",
+        logIndex: 2,
       },
       {
         _tokenId: BigInt(123456789),
         _user: "0x2222222222222222222222222222222222222222",
         _type: 42,
         _homeChain: 2,
-        timestamp: new Date(),
-        blockNumber: 2000,
+        timestamp: new Date(reasonableDate + 2000),
+        blockNumber: reasonableBlock + 2000,
         blockHash: "0xghi789",
         txHash: "0xjkl012",
         logIndex: 2,
